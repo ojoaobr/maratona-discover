2b8351c132a8d0f07a1325eb16df41f5eeec0a99
Joao Pedro <joaopedroaf03@gmail.com>
2021-02-21 16:06:44 -0300

    correção de erro

--- js/index.js
+++ js/index.js
@@ -114,10 +114,9 @@ const DOM = {
 
 const Utils = {
   formatAmount(value){
-    value = Number(value) * 100
-    // value = Number(value.replace(/\,\./g, "")) * 100
+    value = value * 100
 
-    return value
+    return Math.round(value)
   },
 
   formatDate(date){
